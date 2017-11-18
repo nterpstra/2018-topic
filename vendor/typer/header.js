ff22aa2255cb9de90b54d82a038296274fd338bd
rrenode <robert.renodej@gmail.com>
2017-11-18 13:50:40 -0500

    Pushed
    Mobile Nav Next

--- vendor/typer/header.js
+++ vendor/typer/header.js
@@ -1,6 +1,6 @@
 let element = document.querySelector(".header-text-typer");
 typer(element)
-    .line('<span class="type--base">You&#39re in need of a job...</span>')
+    .line('<span class="type--base">You need to land a job...</span>')
     .pause(1500)
 	.line('<span class="type--base">We can help!</span>')
 ;
