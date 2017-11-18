4fef0e22b83fa18adb3d21bb81569f2cb5ccac14
rrenode <robert.renodej@gmail.com>
2017-11-18 11:52:54 -0500

    Pushed

--- vendor/typer/header.js
+++ vendor/typer/header.js
@@ -1,7 +1,6 @@
 let element = document.querySelector(".header-text-typer");
-typer(element).   
-    line('<span class="type--base">You need a job.</span>')
-	.pause(1500)
-	.line('<span class="type--base">We can help you with that!</span>')
-	.end()
+typer(element)
+    .line('<span class="type--base">You&#39re in need of a job...</span>')
+    .pause(1500)
+	.line('<span class="type--base">We can help!</span>')
 ;
